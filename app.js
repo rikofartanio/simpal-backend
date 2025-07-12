446270a6fd27e5f9674655ff748c5656c0b08735
rikofartanio <rikofartanio@gmail.com>
2025-07-12 21:56:39 +0700

    fix: add CORS config for Vercel deployment

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
+// const express = require('express');
+// const cors = require('cors');
+// const serverless = require('serverless-http');
+
+// const app = express();
+// //const port = 3010;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+// //Routes
+// const reportRoute = require('./Routes/report.route');
+// const divisionRoute = require('./Routes/division.route');
+// const authRoutes = require('./Routes/auth.route');
+
+// app.use('/api',reportRoute);
+// app.use('/api', divisionRoute);
+// app.use('/api/auth', authRoutes);
+
+// app.use('/uploads', express.static('uploads'));
+
+// //Init DB
+// const {initDB} = require('./Models');
+// initDB();
+
+// //Server
+// // app.listen(port, ()=>{
+// //     console.log ('simpal server runing on port '+ port)
+// // })
+
+// app.get('/', (req, res) => {
+//   res.send('Simpal Backend API is running');
+// });
+
+// // Export handler untuk Vercel
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
 
 const app = express();
-//const port = 3010;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://simpal-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//Routes
+// Routes
 const reportRoute = require('./Routes/report.route');
 const divisionRoute = require('./Routes/division.route');
 const authRoutes = require('./Routes/auth.route');
 
-app.use('/api',reportRoute);
+app.use('/api', reportRoute);
 app.use('/api', divisionRoute);
 app.use('/api/auth', authRoutes);
 
 app.use('/uploads', express.static('uploads'));
 
-//Init DB
-const {initDB} = require('./Models');
+// Init DB
+const { initDB } = require('./Models');
 initDB();
 
-//Server
-// app.listen(port, ()=>{
-//     console.log ('simpal server runing on port '+ port)
-// })
-
+// Serverless export for Vercel
 app.get('/', (req, res) => {
   res.send('Simpal Backend API is running');
 });
 
-// Export handler untuk Vercel
 module.exports = app;
 module.exports.handler = serverless(app);
